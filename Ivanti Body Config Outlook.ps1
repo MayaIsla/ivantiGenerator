5c6004a87967ef6bacf4a9926aa30d73027b479c
Maya Islambouli <43270477+MayaIsla@users.noreply.github.com>
2022-10-04 14:55:37 -0500

    Update Ivanti Body Config Outlook.ps1

--- Ivanti Body Config Outlook.ps1
+++ Ivanti Body Config Outlook.ps1
@@ -1,13 +1,13 @@
 ﻿
 
 $Body = @{
-    tenant = "becn.saasit.com"
-    username = "misl7603"
-    password = "AmberStandards12#"
-    role = "Admin"
+    tenant = ""
+    username = ""
+    password = ""
+    role = ""
 }
 
-$authCode = Invoke-RestMethod -Method 'POST' -Uri "https://becn.saasit.com/api/rest/authentication/login" -Body $body
+$authCode = Invoke-RestMethod -Method 'POST' -Uri "https://tenant.url/api/rest/authentication/login" -Body $body
 
 $headers = @{
     Authorization=$authCode
@@ -15,12 +15,12 @@ $headers = @{
 }
 
 
-$GetMethod = Invoke-RestMethod -Method 'Get' -Uri "https://becn.saasit.com/api/odata/businessobject/incidents/Current_Support_Services_Tickets?ActionID=54361a74-8ae8-4ac8-a57f-172b06782168" -Headers $headers
+$GetMethod = Invoke-RestMethod -Method 'Get' -Uri "https://becn.saasit.com/api/odata/businessobject/businessobject/NameofSavedSearch?ActionID=ActionIDfromMetaData" -Headers $headers
 
 
 #$outlook = new-object -comobject outlook.application
 #$email = $outlook.CreateItem(0)
-#$email.To = "Maya.Islambouli@becn.com"
-#$email.Subject = "Ivanti JSON Email test"
+#$email.To = "youremail@gmail.com"
+#$email.Subject = ""
 #$email.Body = $GetMethod
 #$email.Send()
